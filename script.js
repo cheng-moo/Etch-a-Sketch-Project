46c0baa8598c8be14c4762e908f93fe00b1fa3ba
Cheng_Mo_ <Cheng_Mo_@gmail.com>
2022-04-06 17:05:04 +0200

    Create Hover State For The Squares

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ for(let i = 0; i < 256; i++) {
     squareDiv.className='square';
     container.appendChild(squareDiv);
 }
+//defining the squareDiv for the global scope
+const squareDivs = document.querySelectorAll('.square');
+//Hover State
+squareDivs.forEach(squareDiv => {
+    squareDiv.addEventListener('mouseenter', hover);
+    squareDiv.addEventListener('mouseleave', hoverEnd)
+})
+function hover (e) {
+    e.target.style.backgroundColor="#d3cfcf";
+}
+function hoverEnd (e) {
+    e.target.style.backgroundColor='';
+}
