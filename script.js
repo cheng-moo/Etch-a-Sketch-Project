e21f6e329113c609e3ce479bf73b1886eac0e2bd
Cheng_Mo_ <Cheng_Mo_@gmail.com>
2022-04-07 02:36:37 +0200

    Create 16x16 grid with clear resize fixed dimentions on the screen features

--- script.js
+++ script.js
@@ -6,18 +6,74 @@ document.body.appendChild(container);
 for(let i = 0; i < 256; i++) {
     const squareDiv = document.createElement('div');
     squareDiv.className='square';
+    let height = 30;
+    let width = height;
+    while (16 * 16 * height * width > 160000) {
+        height--;
+        width = height;
+    }
+    while (16 * 16 * height * width < 160000) {
+        height++;
+        width = height;
+    }
+    squareDiv.style.height = `${height}px`;
+    console.log(squareDiv.style.height);
+    squareDiv.style.width = `${width}px`;
     container.appendChild(squareDiv);
 }
 //defining the squareDiv for the global scope
 const squareDivs = document.querySelectorAll('.square');
 //Hover State
+
 squareDivs.forEach(squareDiv => {
     squareDiv.addEventListener('mouseenter', hover);
     squareDiv.addEventListener('mouseleave', hoverEnd)
+    squareDiv.addEventListener('mousedown', draw);
+   
 })
 function hover (e) {
     e.target.style.backgroundColor="#d3cfcf";
 }
 function hoverEnd (e) {
     e.target.style.backgroundColor='';
+}
+function draw(e) {
+    e.target.classList.add('color');
+}
+const clearBtn = document.querySelector('.clear');
+clearBtn.addEventListener('click', clear);
+function clear () {
+    container.textContent='';
+    let dimensionXY = prompt('What is Your Canvas Dimensions? (e.g  10)', '10');
+    if (dimensionXY > 100) {
+        alert('Maximum 100 squares');
+    } else {
+        
+        for (let i = 0; i < dimensionXY * dimensionXY; i++) {
+            const squareDiv = document.createElement('div');
+            squareDiv.className='square';
+            let height = 30;
+            let width = height;
+            while (dimensionXY * dimensionXY * height * width > 160000) {
+                height--;
+                width = height;
+            }
+            while (dimensionXY * dimensionXY * height * width < 160000) {
+                height++;
+                width = height;
+            }
+            squareDiv.style.height = `${height}px`;
+            console.log(squareDiv.style.height);
+            squareDiv.style.width = `${width}px`;
+            
+            container.appendChild(squareDiv);
+        }
+    container.style.gridTemplateColumns = "repeat("+dimensionXY+",1fr)"
+    
+}
+const squareDivs = document.querySelectorAll('.square');
+squareDivs.forEach(squareDiv => {
+    squareDiv.addEventListener('mouseenter', hover);
+    squareDiv.addEventListener('mouseleave', hoverEnd)
+    squareDiv.addEventListener('mousedown', draw); });
 }
